43639b5c9f626e2f2b22dfcd37e68ecaf1e5d582
fummbly <tstone449@gmail.com>
2024-08-02 09:47:00 -0400

    Started creating classes for information

--- network.py
+++ network.py
@@ -1,5 +1,32 @@
 import socket
 
+class IPNode:
+    def __init__(self):
+        self._alive = False
+        self._ip = ""
+        self._open_ports = []
+
+    def setAlive(self):
+        self._alive = True
+
+    def setIP(self, ip):
+        self._ip = ip
+
+    def addOpenPort(self, open_port):
+        self._open_ports.append(PortNode(open_port))
+
+
+class PortNode:
+    def __init__(self, port = "", banner = ""):
+        self._port = port
+        self._banner = banner
+
+    def addPort(self, port):
+        self._port = port
+
+    def addBanner(self, banner):
+        self._banner = banner
+
 ports = [20, 21, 22, 23, 25, 53, 80, 135, 139, 443, 445,  500, 5060, 8080]
 
 def port_scan(address):
